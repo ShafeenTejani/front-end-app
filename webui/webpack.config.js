45a528fa3077a803e08b0c75796d05ce8aa0de72
Markus Stoy <markus.stoy@timgroup.com>
2017-04-19 15:17:29 +0100

    Shaf/Waz/Markus: get DevServer working by using WebpackHtmlPlugin to generate appropriate index.html during build

--- webui/webpack.config.js
+++ webui/webpack.config.js
@@ -1,10 +1,21 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/main/javascript/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Star Wars',
+            template: 'static/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devServer: {
+        contentBase: "./dist"
     },
     module: {
         loaders: [
